35677d506a0ee36764036dd2405b455be0ae8317
Il Kwon <alexryu1105@naver.com>
2022-09-08 11:47:32 +0900

    [BE] chore : 프로젝트 생성 및 초기 설정

---
+++ backend/src/main/java/com/ssafy/hugging/HuggingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.hugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuggingApplication.class, args);
+	}
+
+}
