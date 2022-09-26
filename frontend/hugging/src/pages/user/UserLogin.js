cbc69975ab97f55752d5b768cc57b35c3a22a72b
Il Kwon <alexryu1105@naver.com>
2022-09-26 23:19:15 +0900

    [FE] refactor : API URL 상수 처리

--- frontend/hugging/src/pages/user/UserLogin.js
+++ frontend/hugging/src/pages/user/UserLogin.js
@@ -1,13 +1,12 @@
-import styled from 'styled-components';
-import styles from './UserLogin.module.css';
-import axios from 'axios'
+import styled from "styled-components";
+import styles from "./UserLogin.module.css";
+import axios from "axios";
 import { BrowserRouter, Router, Route, Switch, Link } from "react-router-dom";
-import KaKaoLogin from 'react-kakao-login'
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useParams } from 'react-router'
+import KaKaoLogin from "react-kakao-login";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router";
 // import { KAKAO_AUTH_URL } from "./OAuth";
-
 
 // let KakaoBtn = styled.button`
 
@@ -26,31 +25,39 @@ import { useParams } from 'react-router'
 // `
 
 function UserLogin() {
+  const navigate = useNavigate();
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-    const navigate = useNavigate()
-    const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-    const REDIRECT_URI =  "http://localhost:3001/redirecturi";
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`; 
-
-
-    return (
-        <>
-        <div className={styles.main_bg}></div>      
-       <div className={styles.header__title}>
-        <h1 className={styles.header__title2}>Hug</h1><h1 className={styles.header__title3}>ging</h1>
-        </div> 
-        <p className={styles.header__text}>언택트 마음챙김 서비스</p>
-        <a className={styles.a_kakao} href={KAKAO_AUTH_URL}>
-        <button className={styles.KakaoBtn} ><p className={styles.kakaoBtn_title}> 카카오 로그인 / 회원가입</p> 
-            <div className={styles.kakao_logo}></div>          
-        </button></a>
-        {/* <Route path="/oauth/kakao/callback">
+  return (
+    <>
+      <div className={styles.main_bg}></div>
+      <div className={styles.header__title}>
+        <h1 className={styles.header__title2}>Hug</h1>
+        <h1 className={styles.header__title3}>ging</h1>
+      </div>
+      <p className={styles.header__text}>언택트 마음챙김 서비스</p>
+      <a className={styles.a_kakao} href={KAKAO_AUTH_URL}>
+        <button className={styles.KakaoBtn}>
+          <p className={styles.kakaoBtn_title}> 카카오 로그인 / 회원가입</p>
+          <div className={styles.kakao_logo}></div>
+        </button>
+      </a>
+      {/* <Route path="/oauth/kakao/callback">
             <Auth />
         </Route> */}
 
-        <button className={styles.counselorBtn} onClick={()=>{ navigate('/counselor/login')}}>상담사 로그인</button>
-        </>
-    )
+      <button
+        className={styles.counselorBtn}
+        onClick={() => {
+          navigate("/counselor/login");
+        }}
+      >
+        상담사 로그인
+      </button>
+    </>
+  );
 }
 
 export default UserLogin;
