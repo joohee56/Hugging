e76a9c89a623f72552b9d64d6d83e439112a51e4
Il kwon <alexryu1105@naver.com>
2022-09-26 14:31:37 +0900

    [FE] fix: 디렉토리 구조

--- frontend/hugging/src/App.js
+++ frontend/hugging/src/App.js
@@ -1,9 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import UserLogin from './pages/user/UserLogin.js'
+import CounselorLogin from './counselor/CounselorLogin';
+import RegisterCategory from './pages/user/RegisterCategory';
+import RegisterProfile from './pages/user/RegisterProfile';
+import Navbar from './components/ui/NavBar';
+import Nav from './components/ui/Nav'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import MyCalendar from './components/counselor/MyCalendar';
+import Location from './components/main/Location';
+import RedirectUri from './pages/user/RedirectUri';
+// import scrollbar from 'smooth-scrollbar';
+
+// // smooth scroll 설정
+// scrollbar.init(document.querySelector('#smooth-scroll'));
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar/>
+      <Nav/> */}
+       {/* <Location></Location> */}
+      <Routes>
+        {/* <MyCalendar></MyCalendar> */}
+        <Route path="/redirecturi" element={<RedirectUri/>}></Route>
+        <Route path="/location" element={<Location/>}></Route>
+        <Route path="/calendar" element={<MyCalendar/>}></Route>
+        <Route path="/login" element={<UserLogin/>} />
+        <Route path="/counselor/login" element={<CounselorLogin/>} />
+        <Route path="/category" element={<RegisterCategory/>} />
+        <Route path="/profile" element={<RegisterProfile/>} />
+        <Route path="/profile_img" element={ <></>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/hugging/src/pages/counselor/CounselorLogin.js
@@ -0,0 +1,64 @@
+import styles from './CounselorLogin.module.css'
+import React from 'react';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginCounselor } from '../store';
+
+function CounselorLogin() {
+
+    let dispatch = useDispatch()
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+ 
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+    
+    let LoginFunc = (e) => {
+        e.preventDefault();
+        if (!inputId) {
+          return alert("ID를 입력하세요.");
+        }
+        else if (!inputPw) {
+          return alert("Password를 입력하세요.");
+        }
+        else {
+            let body = {
+              inputId,
+              inputPw,
+            };
+        
+            axios.post("Endpoint", body)
+            .then((res) => {
+              console.log(res.data);
+              if(res.data.code === 200) {
+                console.log("로그인");
+                dispatch(loginCounselor(res.data.userInfo));
+            }});
+
+    }}
+    return (
+        <div className={styles.counselor}>
+
+        <div className={styles.counselor_title}>
+        <h1 className={styles.header__title2}>Hug</h1><h1 className={styles.header__title3}>ging</h1>
+        </div> 
+        <p className={styles.counselor_text}>상담사 로그인</p>
+        <form id={ styles.login_form} onSubmit={ LoginFunc }>
+            <input type="text" placeholder='Email' onChange={ handleInputId }></input>
+            <input type="text" placeholder='Password' onChange={ handleInputPw } />
+            <p className={styles.forgot_text}>Forgot your password?</p>
+            <button type='submit' className={styles.counselor_login_btn}>로그인</button>
+        </form>
+   
+        
+        </div>
+    )
+}
+
+export default CounselorLogin;
