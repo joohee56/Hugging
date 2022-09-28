50513b4655bcc365106ec15eedbeddd78dd8c055
Il Kwon <alexryu1105@naver.com>
2022-09-29 01:19:37 +0900

    [BE] feat : 상담사 로그인 기능 추가

---
+++ backend/src/main/java/com/ssafy/hugging/counselor/CounselorConstant.java
@@ -0,0 +1,8 @@
+package com.ssafy.hugging.counselor;
+
+public final class CounselorConstant {
+	public static final String COUNSELOR_LOGIN_SUCCESS_MESSAGE = "상담사 로그인 성공";
+
+	public static final String MISMATCH_COUNSELOR_EMAIL_ERROR_MESSAGE = "일치하는 이메일이 없습니다.";
+	public static final String MISMATCH_COUNSELOR_PASSWORD_ERROR_MESSAGE = "일치하는 비밀번호가 없습니다.";
+}
