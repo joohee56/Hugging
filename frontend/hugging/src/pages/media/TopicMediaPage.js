2c9263198b88cfd5cf4712942dbd2c04809c9a29
unknown <sgc5531@gmail.com>
2022-09-30 03:07:16 +0900

    [FE] feat : Topic음악 페이지 추가

---
+++ frontend/hugging/src/pages/media/TopicMediaPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopicMedia from "../../components/media/TopicMedia";
+// import Navbar from '../../components/ui/NavBar'
+// import Nav from '../../components/ui/Nav'
+function TopicMediaPage() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <TopicMedia />
+    </div>
+  );
+}
+
+export default TopicMediaPage;
