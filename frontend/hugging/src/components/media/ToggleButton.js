16a286c1f369653083ae0b00ccb20b5c77580565
unknown <sgc5531@gmail.com>
2022-09-30 03:06:30 +0900

    [FE] style : 코드 스타일 변경

--- frontend/hugging/src/components/media/ToggleButton.js
+++ frontend/hugging/src/components/media/ToggleButton.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import styles from './ToggleButton.module.css'
+import React from "react";
+import styles from "./ToggleButton.module.css";
 
-
-function ToggleButton () {
-
+function ToggleButton() {
   return (
     <>
-    <div className={styles.Box}>
-      <div className={styles.ToggleBox}>
-        <div className={styles.MeditationBox}>
-          <img src='../meditation.png' alt='meditation' className={styles.MeditationImage}></img>
-          <p className={styles.Text}>명상</p>
-        </div>
-        <div className={styles.SoundBox}>
-          <img src='../sound.png' alt='sound' className={styles.SoundImage}></img>
-          <p className={styles.Text}>사운드</p>
+      <div className={styles.Box}>
+        <div className={styles.ToggleBox}>
+          <div className={styles.MeditationBox}>
+            <img
+              src="../meditation.png"
+              alt="meditation"
+              className={styles.MeditationImage}
+            ></img>
+            <p className={styles.Text}>명상</p>
+          </div>
+          <div className={styles.SoundBox}>
+            <img
+              src="../sound.png"
+              alt="sound"
+              className={styles.SoundImage}
+            ></img>
+            <p className={styles.Text}>사운드</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
-
-  
 }
 
 export default ToggleButton;
