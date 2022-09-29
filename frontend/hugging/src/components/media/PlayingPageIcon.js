25535843fe1229ca305e83e6b7a1afb2ce023060
unknown <sgc5531@gmail.com>
2022-09-30 03:01:09 +0900

    [FE] feat : 음악 플레이 페이지 기능연결 및 구조 변경

--- frontend/hugging/src/components/media/PlayingPageIcon.js
+++ frontend/hugging/src/components/media/PlayingPageIcon.js
@@ -1,22 +1,16 @@
-import React from 'react';
-import styles from './PlayingPageIcon.module.css'
+import React from "react";
+import styles from "./PlayingPageIcon.module.css";
 
-
-
-
-
-function PlayingPageIcon () {
+function PlayingPageIcon() {
   return (
     <>
-    <div className={styles.Icon}>
-      <a href='/recommend_media'>
-        <img src='./X.png' alt='image' className={styles.image}></img>
-      </a>
-    </div>
+      <div className={styles.Icon}>
+        <a href="/recommend_media">
+          <img src="./X.png" alt="image" className={styles.image}></img>
+        </a>
+      </div>
     </>
   );
-
-  
 }
 
 export default PlayingPageIcon;
