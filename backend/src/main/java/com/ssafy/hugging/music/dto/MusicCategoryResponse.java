58620dabb13fbe8005b316f9bb17a866a87349dd
Il Kwon <alexryu1105@naver.com>
2022-10-02 02:12:26 +0900

    [BE] feat : 음악 카테고리 제목 추가 및 음악 태그 수정

--- backend/src/main/java/com/ssafy/hugging/music/dto/MusicCategoryResponse.java
+++ backend/src/main/java/com/ssafy/hugging/music/dto/MusicCategoryResponse.java
@@ -12,12 +12,14 @@ import lombok.Getter;
 @Getter
 public class MusicCategoryResponse {
 	private final Integer id;
+	private final String title;
 	private final Category category;
 
 	private final List<MusicResponse> musicResponseList = new ArrayList<>();
 
 	public MusicCategoryResponse(MusicCategory musicCategory) {
 		id = musicCategory.getId();
+		title = musicCategory.getTitle();
 		category = musicCategory.getName();
 		musicResponseList.addAll(
 			musicCategory.getMusicList().stream().map(MusicResponse::new).collect(Collectors.toList()));
