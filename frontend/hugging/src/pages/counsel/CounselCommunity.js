402bed4bd345730b85f7dcf767af6259ab0dcb30
choijoohee213 <choijoohee@naver.com>
2022-10-02 15:43:55 +0900

    [FE] feat: 상담페이지에 유니티 빌드파일 연결
    Former-commit-id: efcc18843ae143869c75106103e12c40a057dc05

--- frontend/hugging/src/pages/counsel/CounselCommunity.js
+++ frontend/hugging/src/pages/counsel/CounselCommunity.js
@@ -1,5 +1,39 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { Link } from "react-router-dom";
+
 const CounselCommunity = () => {
-  return <div>Counsel Community</div>;
+  const { sendMessage, UNSAFE__unityInstance, unityProvider, unload } =
+    useUnityContext({
+      loaderUrl: "Build/WebGLBuild.loader.js",
+      dataUrl: "Build/WebGLBuild.data",
+      frameworkUrl: "Build/WebGLBuild.framework.js",
+      codeUrl: "Build/WebGLBuild.wasm",
+    });
+
+  function send() {
+    window.unityInstance = UNSAFE__unityInstance;
+    sendMessage("HuggingLauncher", "SetCounselInfos", "Community|주희");
+  }
+
+  return (
+    <div>
+      {send()}
+      <Unity
+        style={{
+          width: "90%",
+          height: "100%",
+          justifySelf: "center",
+          alignContent: "center",
+        }}
+        unityProvider={unityProvider}
+      />
+      <button onClick={unload}>
+        <Link to="/counselselect" style={{ textDecoration: "none" }}>
+          나가기
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default CounselCommunity;
