70063fae28f1cd837764046564d0f1f52e0cc256
joohui <joohuii96@gmail.com>
2022-10-03 23:46:59 +0900

    [FE] feat: 메타버스 연결
    Former-commit-id: 31f7b6ea021fa7730144c09785a0be1cf528a8be

--- frontend/hugging/src/App.js
+++ frontend/hugging/src/App.js
@@ -17,11 +17,11 @@ import MusicPlayingPage from "./pages/media/MusicPlayingPage";
 import TopicMediaPage from "./pages/media/TopicMediaPage";
 import ReserveCounsel from "./pages/counsel/ReserveCounsel";
 import SelectCounsel from "./pages/counsel/SelectCounsel";
-import CounselCommunity from "./pages/counsel/CounselCommunity";
+import CounselMetaverse from "./pages/counsel/CounselMetaverse";
 import CounselProfile from "./pages/counsel/CounselProfile";
 import ListCounselor from "./pages/counsel/ListCounselor";
 import MainPage from "./pages/main/MainPage";
-import CounselorMypage from './pages/counselor/CounselorMypage'
+import CounselorMypage from "./pages/counselor/CounselorMypage";
 import CounselDone from "./pages/counsel/CounselDone";
 
 // import scrollbar from 'smooth-scrollbar';
@@ -42,17 +42,20 @@ function App() {
         <Route path="/calendar" element={<MyCalendar />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/counselor/login" element={<CounselorLogin />} />
-        <Route path="/counselor/mypage" element={<CounselorMypage />}/>
+        <Route path="/counselor/mypage" element={<CounselorMypage />} />
         <Route path="/category" element={<RegisterCategory />} />
         <Route path="/profile" element={<RegisterProfile />} />
         <Route path="/profile_img" element={<></>}></Route>
         <Route path="/recommend_media" element={<MediaView />} />
         <Route path="/playing_music:musicId" element={<MusicPlayingPage />} />
-        <Route path="/TopicMediaPage/:categoryId" element={<TopicMediaPage />} />
+        <Route
+          path="/TopicMediaPage/:categoryId"
+          element={<TopicMediaPage />}
+        />
         <Route path="/counselreserve" element={<ReserveCounsel />} />
         <Route path="/" element={<ReserveCounsel />} />
         <Route path="/counselselect" element={<SelectCounsel />}></Route>
-        <Route path="/counselcommunity" element={<CounselCommunity />}></Route>
+        <Route path="/counselmetaverse" element={<CounselMetaverse />}></Route>
         <Route
           path="/counselprofile/:counselorId"
           element={<CounselProfile />}
--- frontend/hugging/src/pages/counsel/CounselMetaverse.js
+++ frontend/hugging/src/pages/counsel/CounselMetaverse.js
@@ -1,9 +1,20 @@
 import classes from "./CounselCommunity.module.css";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import React, { useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const CounselCommunity = () => {
+const CounselMetaverse = () => {
+  const location = useLocation();
+  const from = location.state?.from;
+  const nickName = location.state?.nickName;
+  let counselId = 1;
+  let subject = "Depressed";
+
+  if (from === "OneToOne") {
+    counselId = location.state?.counselId;
+    subject = location.state?.subject;
+  }
+
   const {
     sendMessage,
     UNSAFE__unityInstance,
@@ -35,7 +46,16 @@ const CounselCommunity = () => {
     // AgoraRTC.createStream({ video: false, audio: true });
     // AgoraRTC.getDevices();
     window.unityInstance = UNSAFE__unityInstance;
-    sendMessage("HuggingLauncher", "SetCounselInfos", "Community|주희");
+
+    let msg = "Community|주희";
+    if (from === "Community") {
+      msg = "Community|" + nickName;
+    } else {
+      msg = "OneToOne|" + nickName + "|" + counselId + "|" + subject;
+    }
+
+    console.log(msg);
+    sendMessage("HuggingLauncher", "SetCounselInfos", msg);
   }
 
   useEffect(() => {
@@ -78,4 +98,4 @@ const CounselCommunity = () => {
   );
 };
 
-export default CounselCommunity;
+export default CounselMetaverse;
