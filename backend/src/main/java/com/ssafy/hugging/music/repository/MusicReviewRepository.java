1b60bdc8e96dbeeb3dd8655def0de20dcbdfa13c
Il Kwon <alexryu1105@naver.com>
2022-10-04 05:16:01 +0900

    [BE] fix : 음악 리뷰 등록시 기존 리뷰 업데이트하게 수정
    Former-commit-id: 3cfedf7685c09d16bd96bb7114db5cfec9bc1786

--- backend/src/main/java/com/ssafy/hugging/music/repository/MusicReviewRepository.java
+++ backend/src/main/java/com/ssafy/hugging/music/repository/MusicReviewRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.hugging.music.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.hugging.music.domain.MusicReview;
 
 public interface MusicReviewRepository extends JpaRepository<MusicReview, Integer> {
+	Optional<MusicReview> findMusicReviewByMemberIdAndMusicId(Integer memberId, Integer musicId);
 }
