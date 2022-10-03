f30806923307b4883c67fdf9b4925cd36d369c90
choijoohee213 <choijoohee@naver.com>
2022-10-03 20:22:53 +0900

    [FE] feat: 로딩화면에 로딩 스피너 추가
    Former-commit-id: 6e145c9ca924b956241b5c5aad7f275d8995b131

--- frontend/hugging/src/pages/counsel/CounselCommunity.js
+++ frontend/hugging/src/pages/counsel/CounselCommunity.js
@@ -2,6 +2,8 @@ import classes from "./CounselCommunity.module.css";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import React, { useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
+import { ColorRing } from "react-loader-spinner";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const CounselCommunity = () => {
   const {
@@ -66,7 +68,19 @@ const CounselCommunity = () => {
         // We'll conditionally render the loading overlay if the Unity
         // Application is not loaded.
         <div className={classes.loadingOverlay}>
-          <p>로딩중... ({loadingPercentage}%)</p>
+          <ColorRing
+            visible={true}
+            height="100"
+            width="100"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+          <p>
+            <b>로딩중입니다!</b>
+            <br></br> 조금만 기다려주세요 ☺️<br></br> ({loadingPercentage}%)
+          </p>
         </div>
       )}
       {isLoaded === true && requestFullscreen(true)}
