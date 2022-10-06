7309267c6506afa782094eeef25c6c0b3d5013ce
Il Kwon <alexryu1105@naver.com>
2022-10-06 20:28:40 +0900

    [BE] feat : 회원 탈퇴 추가
    Former-commit-id: dd6aedede15de92769099978c77735e6057d73d8

--- backend/src/main/java/com/ssafy/hugging/member/MemberConstant.java
+++ backend/src/main/java/com/ssafy/hugging/member/MemberConstant.java
@@ -1,6 +1,14 @@
 package com.ssafy.hugging.member;
 
 public class MemberConstant {
+	public static final String GET_FAVORITE_MUSIC_LIST_SUCCESS = "즐겨찾기 음악 조회 성공";
+	public static final String REGISTER_FAVORITE_MUSIC_LIST_SUCCESS = "즐겨찾기 음악 등록 성공";
+	public static final String DELETE_FAVORITE_MUSIC_LIST_SUCCESS = "즐겨찾기 음악 삭제 성공";
+	public static final String GET_FAVORITE_COUNSELOR_LIST_SUCCESS = "즐겨찾기 상담사 조회 성공";
+	public static final String REGISTER_FAVORITE_COUNSELOR_LIST_SUCCESS = "즐겨찾기 상담사 등록 성공";
+	public static final String DELETE_FAVORITE_COUNSELOR_LIST_SUCCESS = "즐겨찾기 음악 삭제 성공";
+	public static final String DELETE_MEMBER_REQUEST_SUCCESS = "회원 탈퇴 성공";
+
 	public static final String MISMATCH_EMAIL_ERROR_MESSAGE = "가입되지 않은 이메일입니다.";
 	public static final String NOT_FOUND_MEMBER_ERROR_MESSAGE = "존재하지 않는 회원입니다.";
 }
