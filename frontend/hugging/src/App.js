ff9bd175ae7c2dac4b2da937611a6d0bdcac18a8
김호진 <hjay1029@naver.com>
2022-10-06 13:40:36 +0900

    [FE] feat: 로그인 여부에 따른 페이지 가입 후 main 이동
    Former-commit-id: 25e262c1f12a3c873fbc6bc8de8ab8c331bcb427

--- frontend/hugging/src/App.js
+++ frontend/hugging/src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UserLogin from "./pages/user/UserLogin.js";
 import CounselorLogin from "./pages/counselor/CounselorLogin";
 import RegisterCategory from "./pages/user/RegisterCategory";
@@ -26,12 +26,16 @@ import MissionStart from "./pages/mission/MissionStart";
 import Timer from "./components/mission/Timer";
 import MissionList from "./pages/mission/MissionList";
 import MissionManagement from "./pages/mission/MissionManagement";
+import { useState } from "react";
 // import scrollbar from 'smooth-scrollbar';
 
 // // smooth scroll 설정
 // scrollbar.init(document.querySelector('#smooth-scroll'));
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => localStorage.getItem("token") || false
+  );
   return (
     <div className="App">
       {/* <Navbar/>
@@ -40,38 +44,81 @@ function App() {
       <Routes>
         {/* <MyCalendar></MyCalendar> */}
         <Route path="/redirecturi" element={<RedirectUri />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/calendar" element={<MyCalendar />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/counselor/login" element={<CounselorLogin />} />
-        <Route path="/counselor/mypage" element={<CounselorMypage />} />
+        <Route
+          path="/counselor/mypage"
+          element={
+            isAuthenticated ? <CounselorMypage /> : <Navigate to="/login" />
+          }
+        />
         <Route path="/category" element={<RegisterCategory />} />
         <Route path="/profile" element={<RegisterProfile />} />
-        <Route path="/profile_img" element={<></>}></Route>
-        <Route path="/recommend_media" element={<MediaView />} />
-        <Route path="/playing_music/:musicId" element={<MusicPlayingPage />} />
+        <Route
+          path="/recommend_media"
+          element={isAuthenticated ? <MediaView /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/playing_music/:musicId"
+          element={
+            isAuthenticated ? <MusicPlayingPage /> : <Navigate to="/login" />
+          }
+        />
         <Route
           path="/TopicMediaPage/:categoryId"
-          element={<TopicMediaPage />}
+          element={
+            isAuthenticated ? <TopicMediaPage /> : <Navigate to="/login" />
+          }
         />
-        <Route path="/counselreserve" element={<ReserveCounsel />} />
+        <Route
+          path="/counselreserve"
+          element={
+            isAuthenticated ? <ReserveCounsel /> : <Navigate to="/login" />
+          }
+        />
         <Route path="/" element={<UserLogin />} />
-        <Route path="/counselselect" element={<SelectCounsel />}></Route>
+        <Route
+          path="/counselselect"
+          element={
+            isAuthenticated ? <SelectCounsel /> : <Navigate to="/login" />
+          }
+        ></Route>
         <Route
           path="/counselprofile/:counselorId"
-          element={<CounselProfile />}
+          element={
+            isAuthenticated ? <CounselProfile /> : <Navigate to="/login" />
+          }
         ></Route>
-        <Route path="/counselreserve" element={<ReserveCounsel />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/counselorlist" element={<ListCounselor />}></Route>
-        <Route path="/counseldone" element={<CounselDone />}></Route>
-        <Route path="/missionstart" element={<MissionStart />}></Route>
-        <Route path="/timer" element={<Timer />}></Route>
-        <Route path="/missionlist" element={<MissionList />}></Route>
+        <Route
+          path="/counselorlist"
+          element={
+            isAuthenticated ? <ListCounselor /> : <Navigate to="/login" />
+          }
+        ></Route>
+        <Route
+          path="/counseldone"
+          element={isAuthenticated ? <CounselDone /> : <Navigate to="/login" />}
+        ></Route>
+        <Route
+          path="/missionstart"
+          element={
+            isAuthenticated ? <MissionStart /> : <Navigate to="/login" />
+          }
+        ></Route>
+        <Route
+          path="/missionlist"
+          element={isAuthenticated ? <MissionList /> : <Navigate to="/login" />}
+        ></Route>
         <Route
           path="/mission/management"
-          element={<MissionManagement />}
+          element={
+            isAuthenticated ? <MissionManagement /> : <Navigate to="/login" />
+          }
         ></Route>
+        <Route
+          path="/main"
+          element={isAuthenticated ? <MainPage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
