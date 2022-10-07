e7b17908389e2599e8a9b72a910a80b722a64afc
joohui <joohuii96@gmail.com>
2022-10-07 09:43:36 +0900

    [FE] refactor: 네브바, 헤더 추가, 오류 수정
    Former-commit-id: 38ac6c6c4312a7d3652a713826f7d2173336da66

--- frontend/hugging/src/pages/counsel/ListCounselor.js
+++ frontend/hugging/src/pages/counsel/ListCounselor.js
@@ -6,6 +6,9 @@ import classes from "./ListCounselor.module.css";
 import TimeCounselorItem from "../../components/counsel/TimeCounselorItem";
 import { useSelector } from "react-redux";
 import Card from "../../components/ui/Card";
+import Nav from "../../components/ui/Nav";
+import NavBar from "../../components/ui/NavBar";
+
 const ListCounselor = () => {
   const location = useLocation();
   const state = location.state;
@@ -94,6 +97,8 @@ const ListCounselor = () => {
 
   return (
     <Fragment>
+      <Nav></Nav>
+      <NavBar></NavBar>
       <CounselListRecommList recommCounselors={counselors} />
       <CounselSubjectList title="전문 분야별" />
       <div>
